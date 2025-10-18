3020e22f178292d1651dc10fc236e68c385a9739
Krisdar9 <104294585+Krisdar9@users.noreply.github.com>
2025-10-18 12:56:48 +0530

    Create NegativeError.cpp

---
+++ NegativeError.cpp
@@ -0,0 +1,12 @@
+#include "format_negative_error.h"
+#include <sstream>
+
+std::string formatNegativeError(const std::vector<int>& negatives) {
+    std::ostringstream err;
+    err << "negatives not allowed: ";
+    for (size_t i = 0; i < negatives.size(); ++i) {
+        err << negatives[i];
+        if (i < negatives.size() - 1) err << ", ";
+    }
+    return err.str();
+}
