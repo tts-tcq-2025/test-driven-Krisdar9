eeb215be18f2a6a23ca84ec3a6b2725561e14479
Krisdar9 <104294585+Krisdar9@users.noreply.github.com>
2025-10-18 12:53:04 +0530

    Create StringCalculator.cpp

---
+++ StringCalculator.cpp
@@ -0,0 +1,30 @@
+#include "string_calculator.h"
+#include "extract_delimiters.h"
+#include "split_numbers.h"
+#include "parse_and_validate.h"
+
+int StringCalculator::add(const std::string& input) {
+    if (input.empty()) return 0;
+
+    std::string numbers = input;
+    std::vector<std::string> delimiters = {",", "\n"};
+
+    if (numbers.rfind("//", 0) == 0) {
+        size_t newlinePos = numbers.find('\n');
+        if (newlinePos == std::string::npos)
+            throw std::invalid_argument("Invalid delimiter declaration.");
+
+        std::string header = numbers.substr(2, newlinePos - 2);
+        delimiters = extractDelimiters(header);
+        numbers = numbers.substr(newlinePos + 1);
+    }
+
+    auto tokens = splitNumbers(numbers, delimiters);
+    auto validNumbers = parseAndValidate(tokens);
+
+    int sum = 0;
+    for (int num : validNumbers)
+        sum += num;
+
+    return sum;
+}
