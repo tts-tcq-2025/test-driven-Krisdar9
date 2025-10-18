fd386893bc22534ac41929a1364c35bdefa3fb47
Krisdar9 <104294585+Krisdar9@users.noreply.github.com>
2025-10-18 13:27:35 +0530

    Update ExtractDelimiters.h

--- ExtractDelimiters.h
+++ ExtractDelimiters.h
@@ -1,9 +1,7 @@
-#ifndef EXTRACTDELIMITERS_H_
-#define EXTRACTDELIMITERS_H_
 
 #include <vector>
 #include <string>
 
 std::vector<std::string> extractDelimiters(const std::string& header);
 
-#endif
+
