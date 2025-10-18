0e8a803588b720fa08c051aac7e1fbaf595d23d8
Krisdar9 <104294585+Krisdar9@users.noreply.github.com>
2025-10-18 13:27:54 +0530

    Update NegativeError.h

--- NegativeError.h
+++ NegativeError.h
@@ -1,9 +1,5 @@
-#ifndef FORMAT_NEGATIVE_ERROR_H
-#define FORMAT_NEGATIVE_ERROR_H
-
 #include <string>
 #include <vector>
 
 std::string formatNegativeError(const std::vector<int>& negatives);
 
-#endif
