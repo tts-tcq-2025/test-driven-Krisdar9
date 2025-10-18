47bf7ac23bc57f0bc65052868dd47cb919107513
Krisdar9 <104294585+Krisdar9@users.noreply.github.com>
2025-10-18 12:56:21 +0530

    Create NegativeError.h

---
+++ NegativeError.h
@@ -0,0 +1,9 @@
+#ifndef FORMAT_NEGATIVE_ERROR_H
+#define FORMAT_NEGATIVE_ERROR_H
+
+#include <string>
+#include <vector>
+
+std::string formatNegativeError(const std::vector<int>& negatives);
+
+#endif
