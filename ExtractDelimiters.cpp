2b7f4d9244c120226416e1a05f9ed79c8eefe16c
Krisdar9 <104294585+Krisdar9@users.noreply.github.com>
2025-10-18 12:54:39 +0530

    Create ExtractDelimiters.cpp

---
+++ ExtractDelimiters.cpp
@@ -0,0 +1,18 @@
+#include "extract_delimiters.h"
+#include <regex>
+
+std::vector<std::string> extractDelimiters(const std::string& header) {
+    std::vector<std::string> delimiters;
+    std::regex multiDelim(R"(\[(.*?)\])");
+
+    auto begin = std::sregex_iterator(header.begin(), header.end(), multiDelim);
+    auto end = std::sregex_iterator();
+
+    for (auto it = begin; it != end; ++it)
+        delimiters.push_back((*it)[1]);
+
+    if (delimiters.empty())
+        delimiters.push_back(header); // single-char delimiter
+
+    return delimiters;
+}
