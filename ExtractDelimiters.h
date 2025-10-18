22457339c1b23385f753917db2c73c07ea476ba4
Krisdar9 <104294585+Krisdar9@users.noreply.github.com>
2025-10-18 12:54:02 +0530

    Create ExtractDelimiters.h

---
+++ ExtractDelimiters.h
@@ -0,0 +1,9 @@
+#ifndef EXTRACT_DELIMITERS_H
+#define EXTRACT_DELIMITERS_H
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> extractDelimiters(const std::string& header);
+
+#endif
