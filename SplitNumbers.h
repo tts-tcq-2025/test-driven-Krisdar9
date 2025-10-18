1490ba9473ac100fa1bec7e67c9758f27a50bbd0
Krisdar9 <104294585+Krisdar9@users.noreply.github.com>
2025-10-18 13:26:57 +0530

    Update SplitNumbers.h

--- SplitNumbers.h
+++ SplitNumbers.h
@@ -1,9 +1,5 @@
-#ifndef SPLITNUMBERS_H_
-#define SPLITNUMBERS_H_
 
 #include <vector>
 #include <string>
 
 std::vector<std::string> splitNumbers(const std::string& input, const std::vector<std::string>& delimiters);
-
-#endif
