d09d5a1ad0ddf328ba34221e772c5268cbeceb7e
Krisdar9 <104294585+Krisdar9@users.noreply.github.com>
2025-10-18 12:58:06 +0530

    Create ParseandValidate.h

---
+++ ParseandValidate.h
@@ -0,0 +1,9 @@
+#ifndef PARSE_AND_VALIDATE_H
+#define PARSE_AND_VALIDATE_H
+
+#include <string>
+#include <vector>
+
+std::vector<int> parseAndValidate(const std::vector<std::string>& tokens);
+
+#endif
